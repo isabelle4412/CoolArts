d0692782425aaf0f3833155fc2b386e23b199aef
Isabelle Pouget <isab.pouget@gmail.com>
2020-08-17 18:38:22 +0200

    destroy user

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,10 +1,10 @@
 class Painting < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   validates :name, :description, :address, :price_per_day, presence: :true
   validates :name, uniqueness: true
 
-  validates :price_per_day, numericality: { only_integer: true }
+  validates :price_per_day, numericality: true
 
 end
--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-    has_many :paintings
-    has_many :bookings
-
-    validates :first_name, :last_name, presence: :true
-    validates :first_name, uniqueness: { scope: :last_name }
-
-end

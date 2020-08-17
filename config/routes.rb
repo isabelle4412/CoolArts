6317ae6c889a66c3b9364ab50f5b8f1f1a9160ed
Tommie Petit <tommiepetit@hotmail.fr>
2020-08-17 16:41:55 +0200

    routes create

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :paintings do
-    resources :bookings, only:[:index, :destroy]
+  resources :paintings, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:index]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :paintings , only: [:index, :show, :new, :create]
-  resources :bookings, only: [:index, :create, :new]
+
+  namespace :owner do
+    resources :paintings, only: [:index]
+  end
+
+  resources :bookings, only: [:index, :create, :new] do
+    member do
+      patch 'accept'
+      patch 'decline'
+    end
+  end
 end
+

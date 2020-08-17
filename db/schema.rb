ef7fb559e386590c857e9f800b5c0c7920f14dc6
Isabelle Pouget <isab.pouget@gmail.com>
2020-08-17 18:42:57 +0200

    Correct models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_160921) do
+ActiveRecord::Schema.define(version: 2020_08_17_153356) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,10 +51,7 @@ ActiveRecord::Schema.define(version: 2020_08_17_160921) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "bookings", "paintings"
   add_foreign_key "bookings", "users"
   add_foreign_key "paintings", "users"
-  
-
 end

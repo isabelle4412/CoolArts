99af8e386d18c0a4144213a60659e2b551df9024
Tommie Petit <tommiepetit@hotmail.fr>
2020-08-17 16:09:39 +0200

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :paintings do
+    resources :bookings, only:[:index, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :paintings , only: [:index, :show, :new, :create]
+  resources :bookings, only: [:index, :create, :new]
 end

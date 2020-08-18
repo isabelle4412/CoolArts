7e1889a6d0641e4cc3e4d0f94bbad77955510b48
Zoe Pralus <zoe.pralus@audencia.com>
2020-08-18 12:44:31 +0200

    seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+# Booking.destroy_all
+# Painting.destroy_all
+User.destroy_all
+
+puts "Creating users.."
+
+tommie = {email: "tom@mail.com", encrypted_password: "password", first_name: "Tommie" , last_name: "Petit" }
+isabelle = {email: "isabelle@mail.com", encrypted_password: "password", first_name: "Isabelle" , last_name: "Pouget" }
+yohann = {email: "yohann@mail.com", encrypted_password: "password", first_name: "Yohann" , last_name: "Bonamy" }
+zoe = {email: "zoe@mail.com", encrypted_password: "password", first_name: "Zoé" , last_name: "Pralus" }
+
+[tommie, isabelle, yohann, zoe ].each do |attributes|
+  user = User.create(attributes)
+  puts "Created #{user.first_name}"
+
+end
+puts "Finished!"
+
+
+puts "Creating paintings.."
+
+
+
+[tommie, isabelle, yohann, zoe ].each do |attributes|
+  painting = Painting.create(attributes)
+  puts "Created #{user.first_name}"
+
+end
+

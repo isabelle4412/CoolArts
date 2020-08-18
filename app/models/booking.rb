c0c93e92d785e4085a818682e9d04054d86f0531
Isabelle Pouget <63108055+isabelle4412@users.noreply.github.com>
2020-08-18 15:32:18 +0200

    Revert "Seed"

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,10 +4,11 @@ class Booking < ApplicationRecord
 
   validates :starts_on, :ends_on, :total_price, :state, presence: true
 
-  validates :starts_on, :ends_on, presence: true
+  validates :starts_on, :ends_on, availability: true
+  validate :end_date_after_start_date
 
   validates :total_price, numericality: true
 
-  validates :state, acceptance: { accept: ["accepted", "refused", "pending"] }
+  validates :state, acceptance: { accept: [“accepted”, “refused”, “pending”] }
 
 end

37b9ea902fe1f80ba58b592c72c131c9657b546e
Isabelle Pouget <isab.pouget@gmail.com>
2020-08-18 11:32:58 +0200

    Finished model

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,6 +1,6 @@
 class Painting < ApplicationRecord
   belongs_to :user
-  has_many :bookings, dependent: :destroy
+  has_many :bookings
 
   validates :name, :description, :address, :price_per_day, presence: :true
   validates :name, uniqueness: true

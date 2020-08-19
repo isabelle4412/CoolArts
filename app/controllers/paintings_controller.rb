487fb38db6c5e6409513bee08749d0fa061669d5
Zoe Pralus <zoe.pralus@audencia.com>
2020-08-19 14:37:54 +0200

    finish the design and creation of the form for new painting

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -7,5 +7,25 @@ class PaintingsController < ApplicationController
     def show
         @painting = Painting.find(params[:id])
     end
-    
+
+  def new
+    @painting = Painting.new
+  end
+
+  def create
+    @painting = Painting.new(painting_params)
+    @painting.user = User.where(first_name: current_user.first_name).first
+
+    if @painting.save
+      redirect_to @painting, notice: 'Painting was successfully added.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def painting_params
+    params.require(:painting).permit(:name, :description, :price_per_day, :address, :photo)
+  end
 end

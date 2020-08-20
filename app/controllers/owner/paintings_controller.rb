b5efe2b443e8d64704a1132b2d93de74d297beca
Isabelle Pouget <isab.pouget@gmail.com>
2020-08-20 18:16:18 +0200

    Owner Painting creation

---
+++ app/controllers/owner/paintings_controller.rb
@@ -0,0 +1,9 @@
+module Owner 
+    class PaintingsController < ApplicationController
+        def index
+            @owner_paintings = current_user.paintings
+        end
+        
+    end 
+
+end

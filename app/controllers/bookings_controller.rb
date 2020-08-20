2589b11dbae9b859ae4aba5d427bdabd569edc6b
Zoe Pralus <zoe.pralus@audencia.com>
2020-08-20 12:43:56 +0200

    récupération show pour poursuite formulaire

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,21 @@
 class BookingsController < ApplicationController
   def index
   end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = User.where(first_name: current_user.first_name).first
+
+    if @booking.save
+      redirect_to @painting, notice: 'La réservation a bien été envoyé'
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def painting_params
+    params.require(:booking).permit(:starts_on, :ends_on)
+  end
 end

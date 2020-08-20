3f94646d0e3e3481c9353d58b5275815fd463692
Bonamy Yoann <yoann.bonamy79@gmail.com>
2020-08-20 16:01:31 +0200

    card_reservation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,22 +3,9 @@ class BookingsController < ApplicationController
 
   def index
     @bookings = current_user.bookings
+
     # @paintings = Painting.all
     # # @paintsing = Painting.find(params[:painting_id])
     # @bookings.painting = cl_image_path painting.photo.key
-  end
-
-  def new
-    @bookings = Booking.new
-  end
-
-  def create
-    @booking = Booking.new(booking_params)
-  end
-
-  private
-
-  def booking_params
-     params.require(:booking).permit(:starts_on, :ends_on, :state, :total_price)
   end
 end

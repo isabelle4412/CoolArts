bc5a82540c271b8fc9496c81fd28fbc9755b8a14
Zoe Pralus <zoe.pralus@audencia.com>
2020-08-21 15:29:50 +0200

    ordre corrigé

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,7 +2,7 @@ class BookingsController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:index, :new]
 
   def index
-    @bookings = current_user.bookings
+    @bookings = current_user.bookings.order(created_at: :desc)
     # @paintings = Painting.all
     # # @paintsing = Painting.find(params[:painting_id])
     # @bookings.painting = cl_image_path painting.photo.key
